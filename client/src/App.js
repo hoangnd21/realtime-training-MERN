696e7eb78f22f1e24eaf0891e97c745a49873f61
hoangnd21 <49250781+hoangnd21@users.noreply.github.com>
2019-12-18 11:57:20 +0700

    waiting lau vl

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [realtime, setRealtime] = useState(null)
+  useEffect(() => {
+    axios.get('')
+      .then(res => {
+
+      })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +18,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p></p>
       </header>
     </div>
   );
