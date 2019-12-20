75f8e41989c5704b10daac79c4d418deb24b15d0
hoangnd21 <49250781+hoangnd21@users.noreply.github.com>
2019-12-20 15:32:39 +0700

    fe to be complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios'
+import {
+  Input,
+  Row,
+  Col,
+} from 'antd'
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:8000');
 
 function App() {
-  const [realtime, setRealtime] = useState(null)
-  useEffect(() => {
-    axios.get('')
-      .then(res => {
 
-      })
-  }, [])
+  const [defaultMessage, setDefaultMessage] = useState(null)
+  const sendMessage = e => {
+    socket.emit('react_message', e.target.value);
+    setDefaultMessage(null)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p></p>
       </header>
+      <Row style={{ margin: 10, padding: 10, border: '1px solid #34558B', borderRadius: 5 }}>
+        <Col xl={6}>
+          nav
+        </Col>
+        <Col xl={12} style={{ borderRight: '1px solid #34558B', borderLeft: '1px solid #34558B', padding: 10 }}>
+          <p style={{ textAlign: 'left', minHeight: '55vh' }}>
+            chatbox<br />
+          </p>
+          <div style={{ verticalAlign: 'bottom' }}>
+            <Input onPressEnter={sendMessage} placeholder='Enter something...' value={defaultMessage} />
+          </div>
+        </Col>
+        <Col xl={6}>
+          nav
+        </Col>
+      </Row>
     </div>
   );
 }
